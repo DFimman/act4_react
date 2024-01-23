685c9ed215cb0a456292fdccccdb3d947db3ba64
DFimman <dichosaimmanuel@gmail.com>
2024-01-23 16:24:35 +0800

    added functions (add, delete, toggle)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,44 @@ import { RiCheckboxBlankCircleLine, RiCheckboxCircleLine, RiDeleteBinLine } from
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
+const TodoItem = ({ todo, onDelete, onToggle }) => {
+  return (
+    <div className="todo-item">
+      <span onClick={() => onToggle(todo.id)}>
+        {todo.completed ? <RiCheckboxCircleLine /> : <RiCheckboxBlankCircleLine />}
+      </span>
+      <p className={todo.completed ? 'completed' : ''}>{todo.text}</p>
+      <span onClick={() => onDelete(todo.id)} className="delete-icon">
+        <RiDeleteBinLine />
+      </span>
+    </div>
+  );
+};
 
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos((prevTodos) => [...prevTodos, { id: uuidv4(), text: newTodo, completed: false }]);
+      setNewTodo('');
+    }
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  
+};
 
 export default App;
