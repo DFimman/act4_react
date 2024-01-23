27ad65aeef2644ac2c82473f26ea44b70b724131
DFimman <dichosaimmanuel@gmail.com>
2024-01-23 16:23:44 +0800

    Downloaded and imported need libraries
    react icons and uuid

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { RiCheckboxBlankCircleLine, RiCheckboxCircleLine, RiDeleteBinLine } from 'react-icons/ri';
+import { v4 as uuidv4 } from 'uuid';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>This is a To do List React Application</h1>
-      <div className="App">
-        {/* <TodoComponent /> */} This is todo component
-        <div>
-        {/* <Socialcomponent/> */} This is Social components
-        </div>
-      </div>
-    </>
-  )
-}
 
-export default App
+export default App;
